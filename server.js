7ce1f9fcd666223b6a45484f544658fac923beb5
laiqbudye <laiqbudye@gmail.com>
2020-06-14 07:25:51 +0530

    Production || production related changes

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv');
 const colors = require('colors');
 const morgan = require('morgan');
 const connectDB = require('./config/db');
+const path = require('path');
 
 dotenv.config({ path: './config/config.env'});
 
@@ -19,6 +20,16 @@ if(process.env.NODE_ENV === 'development'){
 }
 
 app.use('/api/v1/transactions', transactions);
+
+// serve static assets in production 
+if(process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'));
+    
+    app.get('*', (req,res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
